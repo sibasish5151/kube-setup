db801b5a576a791a12980864e2221f0b1b2ed84f
sibasish5151 <sibasish5151@gmail.com>
2024-04-25 06:22:14 +0000

    usernodesetupfile-commit

---
+++ usernodesetup.sh
@@ -0,0 +1,7 @@
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+kubectl version
+mkdir ~/.kube
+vi ~/.kube/config
